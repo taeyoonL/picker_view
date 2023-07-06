c5b7ac0122f89354ba73f7efd09b6a45fb32929c
taeyoonL <tylee3715@gmail.com>
2023-07-06 11:10:01 +0900

    Initial Commit

---
+++ picker_view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  picker_view
+//
+//  Created by 이태윤 on 2023/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ picker_viewUITests/picker_viewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  picker_viewUITestsLaunchTests.swift
+//  picker_viewUITests
+//
+//  Created by 이태윤 on 2023/07/06.
+//
+
+import XCTest
+
+final class picker_viewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
